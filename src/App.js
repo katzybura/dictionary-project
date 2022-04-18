81a4e8f381eaa8283217bf97c688d510665f3b65
katzybura <97856084+katzybura@users.noreply.github.com>
2022-04-18 17:27:23 -0400

    changed styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-  <h1>Dictionary Search</h1>
+  <h1>Dictionary Search 📖</h1>
       </header>
       <Dictionary />
       <footer><a href="https://github.com/katzybura/dictionary-project" target="_blank" rel="noreferrer">Open Source Code</a> by Katherine Zybura</footer>
