fff5828f3a6593764f3aed7a5388f409eada9e69
katzybura <97856084+katzybura@users.noreply.github.com>
2022-04-17 20:59:50 -0400

    created form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-  <p>Hello</p> 
+  <h1>Dictionary</h1>
       </header>
+      <Dictionary />
+      <footer>Open Source Code by Katherine Zybura</footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+
+export default function Dictionary(){
+    let [keyword, setKeyword]= useState("");
+    function search(event){
+        event.preventDefault();
+        alert(`Searching for ${keyword}`);
+    }
+    function handleKeywordChange(event){
+setKeyword(event.target.value);
+    }
+    return(
+        <div className="Dictionary">
+<form onSubmit={search}>
+    <input type="search" placeholder="Search for a word" onChange={handleKeywordChange}/>
+    <input type="submit" value="Search"/> 
+</form>
+        </div>
+    );
+}
