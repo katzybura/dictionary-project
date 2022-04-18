f224dc8b2739ec64d275d1c34fea95084c373a38
katzybura <97856084+katzybura@users.noreply.github.com>
2022-04-18 15:38:35 -0400

    fixed link

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ import "./Phonetic.css";
 export default function Phonetic(props){
 return(
     <div className="Phonetic">
-        <a href={props.phonetic.audio} target="_blank" className="ListenLink">
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer" className="ListenLink">
             🔊
         </a>
         {props.phonetic.text}
